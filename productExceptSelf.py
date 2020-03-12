4ef0f34e6b5183d3335f6e937a3d225ab09e8552
Tuấn Nguyễn <20045259+tnguyenminh@users.noreply.github.com>
2020-03-12 14:34:18 +0700

    Update productExceptSelf.py

--- productExceptSelf.py
+++ productExceptSelf.py
@@ -1,4 +1,4 @@
-def productExceptSelf(self, nums: List[int]) -> List[int]:
+def productExceptSelf(nums):
     """Product of Array Except Self
     Given an array nums of n integers where n > 1, return an array output such that
     output[i] is equal to the product of all the elements of nums except nums[i].
